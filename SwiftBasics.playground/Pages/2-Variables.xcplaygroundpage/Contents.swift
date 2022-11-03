399b6218faa48b5cf9ce4debad528348bdf941e8
ercelserhat <ercelserhat@gmail.com>
2022-11-03 22:45:50 +0300

    Variables

---
+++ SwiftBasics.playground/Pages/2-Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var ogrenciAd = "Serhat"
+var ogrenciYas = 30
+var ogrenciBoy = 1.82
+var ogrenciBasHarf = "S"
+
+print(ogrenciAd)
+print(ogrenciYas)
+print(ogrenciBoy)
+print(ogrenciBasHarf)
+
+var urunId:Int = 3416
+var urunAd:String = "Kol Saati"
+var urunAdet:Int = 100
+var urunFiyat:Double = 109.99
+var urunTedarikci:String = "Rolex"
+
+print(urunId)
+print(urunAd)
+print(urunAdet)
+print(urunFiyat)
+print(urunTedarikci)
