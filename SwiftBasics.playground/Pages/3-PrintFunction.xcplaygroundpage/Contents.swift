144e43cf71a56b81cc6a8e8d77c23dd79d92775a
ercelserhat <ercelserhat@gmail.com>
2022-11-03 23:14:42 +0300

    Function of print

---
+++ SwiftBasics.playground/Pages/3-PrintFunction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var isim:String = "Serhat"
+var yas:Int = 30
+var sehir:String = "Ankara"
+
+print(isim, yas, sehir)
+
+print(isim, yas, sehir, separator: " - ")
+
+print("İsim: \(isim), Yaş: \(yas), Şehir: \(sehir)")
